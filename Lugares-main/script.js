385b6aa1532f382df7767d31fd9d8bc99d583373
Matheus-hfv <119900495+Matheus-hfv@users.noreply.github.com>
2024-09-09 21:00:59 -0300

    Add files via upload

---
+++ Lugares-main/script.js
@@ -0,0 +1,20 @@
+
+  function mostrarAlertacad(){
+    var nomeCadastro = document.getElementById('username').value;
+    alert(`Cadastro de ${nomeCadastro} bem-sucedido!`);
+  }
+
+  
+function mostrarAlerta() {
+    var nome = document.getElementById('nome').value;
+    
+      alert(`Login de ${nomes} bem-sucedido!`);
+    
+  };
+
+  if(typeof navigator.serviceWorker !=='undefined'){
+    navigator.serviceWorker.register('pwabuilder-sw.js')
+
+  }
+
+  
